a4832061e50b0788f9ec166f553fc49da439e406
akhila <akhiladharavath69@gmail.com>
2023-10-23 19:54:40 +0530

    first commit

---
+++ demo/cache/9dfea230743b82923b990beffd3452be9250c7e3.index.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 4.3.2, created on 2023-08-05 14:08:44
+  from 'C:\xampp\htdocs\Smarty\demo\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.2',
+  'unifunc' => 'content_64ce3bcc56d5f0_57469382',
+  'has_nocache_code' => true,
+  'file_dependency' => 
+  array (
+    '8928953303f6deecbc0a667cff36e76de8ec086d' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Smarty\\demo\\templates\\index.tpl',
+      1 => 1691237226,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 120,
+),true)) {
+function content_64ce3bcc56d5f0_57469382 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['title']->value;?>
+
+ 
+ <?php }
+}
---
+++ demo/templates_c/8928953303f6deecbc0a667cff36e76de8ec086d_0.file.index.tpl.cache.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 4.3.2, created on 2023-08-05 14:08:44
+  from 'C:\xampp\htdocs\Smarty\demo\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.2',
+  'unifunc' => 'content_64ce3bcc5591c8_39261646',
+  'has_nocache_code' => true,
+  'file_dependency' => 
+  array (
+    '8928953303f6deecbc0a667cff36e76de8ec086d' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Smarty\\demo\\templates\\index.tpl',
+      1 => 1691237226,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64ce3bcc5591c8_39261646 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '113282522064ce3bcc4fdfc6_97570930';
+?>
+
+<?php echo '/*%%SmartyNocache:113282522064ce3bcc4fdfc6_97570930%%*/<?php echo $_smarty_tpl->tpl_vars[\'title\']->value;?>
+/*/%%SmartyNocache:113282522064ce3bcc4fdfc6_97570930%%*/';?>
+
+ 
+ <?php }
+}
